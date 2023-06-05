3f7b84aa2698f2655d2b18f3888ea7ded7f43147
Trifonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-06-05 16:44:54 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/style.css';
+const API_KEY = '954f5a062d83196561f460329e10940e';
+const www = 'https://api.openweathermap.org/data/3.0/onecall';
+const options = new URLSearchParams({
+  lat: 1,
+  lon: 1,
+  exclude: 'curent',
+  units: 'metric',
+  appid: API_KEY,
+});
 
-console.log(getCurrentPosition());
+fetch(`${www}?${options}`)
+  .then(resp => resp.json())
+  .then(resp => console.log(resp));
+
+const qwe = document.querySelector('.qwe');
+
+function getLocation() {
+  var geolocation = navigator.geolocation;
+  geolocation.getCurrentPosition(showLocation);
+}
+
+function showLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  console.log(latitude);
+  console.log(longitude);
+  return { latitude, longitude };
+}
+getLocation();
